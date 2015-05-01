efdcd25e09da8c7e56b846786721298501819d95
Nick Van Dyck <nickvandyck@NextBook.local>
2015-05-01 16:21:42 +0200

    update brew commands

--- brew.sh
+++ brew.sh
@@ -22,7 +22,7 @@ brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed
 brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`
-brew install gnu-sed --default-names
+brew install gnu-sed --with-default-names
 
 # Install Bash 4.
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
@@ -34,5 +34,5 @@ brew install bash-completion2
 #Install node
 brew install node
 
-#Keeping the house clean
+#Cleanup the house
 brew cleanup
