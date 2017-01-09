fceb4dbbf6d79e2c3abba4a4a4c36da44419e730
nickvdyck <vandyck.nick@outlook.com>
2017-01-09 16:45:46 +0000

    fix redundant space at end of file

--- src/unix/bootstrap.sh
+++ src/unix/bootstrap.sh
@@ -6,7 +6,7 @@ git pull origin master;
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE" --exclude "pre-bootstrap.sh" \
-		--exclude "brew.sh" --exclude "brew-cask.sh" --exclude "src/"  \ 
+		--exclude "brew.sh" --exclude "brew-cask.sh" --exclude "src/" \
 		-avh --no-perms . ~;
 	source ~/.bash_profile;
 }
