ce0b541ee8645ce3681a9f3b4090c737abb1b27e
Nick Van Dyck <vandyck.nick@outlook.com>
2021-03-03 23:57:21 +0100

    feat: clipboard alias

--- git/configure.sh
+++ git/configure.sh
@@ -1,6 +1,5 @@
 git config --global user.name "Nick Van Dyck"
 git config --global user.email "vandyck.nick@outlook.com"
 git config --global commit.gpgsign true
-
 # gpg --list-secret-keys --keyid-format LONG
-# git config --global user.signingkey KEY
+git config --global user.signingkey 05B48F7FBCDCC54E
