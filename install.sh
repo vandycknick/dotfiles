5b975ddfc007e141eb0f3eff06415acb81adc9ed
Nick Van Dyck <vandyck.nick@outlook.com>
2021-05-31 22:36:33 +0200

    chore: update nvm

--- install.sh
+++ install.sh
@@ -82,7 +82,7 @@ fi
 
 echo ""
 echo "Installing nvm"
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.37.2/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.38.0/install.sh | bash
 
 echo ""
 echo "Installing yarn"
