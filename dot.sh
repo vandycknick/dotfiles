4114909d3ae7004e76ac74a521c08ed87b0b482c
Nick Van Dyck <vandyck.nick@outlook.com>
2021-08-21 00:03:36 +0200

    fix: macos homebrew install script

--- dot.sh
+++ dot.sh
@@ -5,57 +5,55 @@ case "$(uname -s)" in
     Darwin)
         # install Command Line Tools
         if [[ ! -x /usr/bin/gcc ]]; then
-          echo "[i] Install macOS Command Line Tools"
-          xcode-select --install
+            echo "[i] Installing macOS Command Line Tools"
+            xcode-select --install
         fi
 
         # install homwbrew
         if [[ ! -x /usr/local/bin/brew ]]; then
-          echo "[i] Install Homebrew"
-          ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+            echo "[i] Installing Homebrew"
+            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
         fi
 
         # install ansible
         if [[ ! -x /usr/local/bin/ansible ]]; then
-            echo "[i] Install Ansible"
+            echo "[i] Installing Ansible"
             brew install ansible
         fi
-
         ;;
 
     Linux)
-        if [ -f /etc/os-release ]
-            then
-                echo "[i] Ask for sudo password"
-                sudo -v
+        if [ -f /etc/os-release ]; then
+            echo "[i] Ask for sudo password"
+            sudo -v
 
-                . /etc/os-release
+            . /etc/os-release
 
-                case "$ID" in
-                    debian | ubuntu | pop | kali)
-                        if [[ ! -x $(command -v ansible) ]]; then
-                            echo "[i] Install Ansible"
-                            sudo apt-get update
-                            sudo apt-get install -y ansible
-                        fi
-                        ;;
+            case "$ID" in
+                debian | ubuntu | pop | kali)
+                    if [[ ! -x $(command -v ansible) ]]; then
+                        echo "[i] Install Ansible"
+                        sudo apt-get update
+                        sudo apt-get install -y ansible
+                    fi
+                    ;;
 
-                    arch)
-                        if [[ ! -x $(command -v ansible) ]]; then
-                            echo "[i] Install Ansible"
-                            sudo pacman -S ansible --noconfirm
-                        fi
-                        ;;
+                arch)
+                    if [[ ! -x $(command -v ansible) ]]; then
+                        echo "[i] Install Ansible"
+                        sudo pacman -S ansible --noconfirm
+                    fi
+                    ;;
 
-                    *)
-                        echo "[!] Unsupported Linux Distribution: $ID"
-                        exit 1
-                        ;;
-                esac
-            else
-                echo "[!] Unsupported Linux Distribution"
-                exit 1
-            fi
+                *)
+                    echo "[!] Unsupported Linux Distribution: $ID"
+                    exit 1
+                    ;;
+            esac
+        else
+            echo "[!] Unsupported Linux Distribution"
+            exit 1
+        fi
         ;;
 
     *)
