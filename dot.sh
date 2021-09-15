646a9dca316ff785884f9d96993b3057cb69cb09
Nick Van Dyck <vandyck.nick@outlook.com>
2021-09-15 09:21:30 +0200

    feat: improve dot sh bootstrap script

--- dot.sh
+++ dot.sh
@@ -1,69 +1,122 @@
-#!/usr/bin/env bash
-set -e
+#!/bin/sh
 
-case "$(uname -s)" in
-    Darwin)
-        # install Command Line Tools
-        if [[ ! -x /usr/bin/gcc ]]; then
-            echo "[i] Installing macOS Command Line Tools"
+set -eu
+
+print_version() {
+    return "1.0.0"
+}
+
+usage() {
+    cat 1>&2 <<EOF
+dot 1.0.0
+The installer for my dotfiles
+
+USAGE:
+    rustup-init [FLAGS] [OPTIONS]
+
+FLAGS:
+    -v, --verbose           Enable verbose output
+    -h, --help              Prints help information
+    -V, --version           Prints version information
+
+OPTIONS:
+    -t, --tags <tags>       Only install specified tags.
+EOF
+}
+
+main() {
+    local verbose=0
+
+    for arg in "$@"; do
+        case "$arg" in
+            -h|--help)
+                usage
+                exit 0
+                ;;
+            -V|--version)
+                print_version
+                exit 0 
+                ;;
+            -v|--verbose)
+                verbose=1
+                ;;
+            *)
+                ;;
+        esac
+    done
+
+    echo "[i] Checking prereqs"
+    local ostype="$(uname -s)"
+
+    case "$ostype" in
+        Darwin)
+            # install Command Line Tools
+            if [[ ! -x /usr/bin/gcc ]]; then
+            echo "[i] Install macOS Command Line Tools"
             xcode-select --install
-        fi
+            fi
 
-        # install homwbrew
-        if [[ ! -x /usr/local/bin/brew ]]; then
-            echo "[i] Installing Homebrew"
-            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-        fi
+            # install homwbrew
+            if [[ ! -x /usr/local/bin/brew ]]; then
+            echo "[i] Install Homebrew"
+            ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+            fi
 
-        # install ansible
-        if [[ ! -x /usr/local/bin/ansible ]]; then
-            echo "[i] Installing Ansible"
-            brew install ansible
-        fi
-        ;;
+            # install ansible
+            if [[ ! -x /usr/local/bin/ansible ]]; then
+                echo "[i] Install Ansible"
+                brew install ansible
+            fi
 
-    Linux)
-        if [ -f /etc/os-release ]; then
-            echo "[i] Ask for sudo password"
-            sudo -v
+            ;;
 
-            . /etc/os-release
+        Linux)
+            if [ -f /etc/os-release ]
+                then
+                    echo "[i] Ask for sudo password"
+                    sudo -v
 
-            case "$ID" in
-                debian | ubuntu | pop | kali)
-                    if [[ ! -x $(command -v ansible) ]]; then
-                        echo "[i] Install Ansible"
-                        sudo apt-get update
-                        sudo apt-get install -y ansible
-                    fi
-                    ;;
+                    . /etc/os-release
 
-                arch)
-                    if [[ ! -x $(command -v ansible) ]]; then
-                        echo "[i] Install Ansible"
-                        sudo pacman -S ansible --noconfirm
-                    fi
-                    ;;
+                    case "$ID" in
+                        debian | ubuntu | pop | kali)
+                            if [ ! -x $(command -v ansible) ]; then
+                                echo "[i] Install Ansible"
+                                sudo apt-get update
+                                sudo apt-get install -y ansible
+                            fi
+                            ;;
 
-                *)
-                    echo "[!] Unsupported Linux Distribution: $ID"
+                        arch)
+                            if [ ! -x $(command -v ansible) ]; then
+                                echo "[i] Install Ansible"
+                                sudo pacman -S ansible --noconfirm
+                            fi
+                            ;;
+
+                        *)
+                            echo "[!] Unsupported Linux Distribution: $ID"
+                            exit 1
+                            ;;
+                    esac
+                else
+                    echo "[!] Unsupported Linux Distribution"
                     exit 1
-                    ;;
-            esac
-        else
-            echo "[!] Unsupported Linux Distribution"
+                fi
+            ;;
+
+        *)
+            echo "[!] Unsupported OS"
             exit 1
-        fi
-        ;;
+            ;;
+    esac
 
-    *)
-        echo "[!] Unsupported OS"
-        exit 1
-        ;;
-esac
 
-# Run main playbook
-echo "[i] Run Playbook"
-ansible-playbook -i hosts dotfiles.yml --ask-become-pass
+    # Run main playbook
+    echo "[i] Run Playbook"
+    ansible-playbook -i hosts dotfiles.yml --ask-become-pass
 
-echo "[i] Done."
+    echo "[i] Done."
+}
+
+main "$@" || exit 1
