468902a04c59693e16827b80c106e65db14a4e44
Nick Van Dyck <vandyck.nick@outlook.com>
2024-07-24 13:02:16 +0200

    update(nvim): Make conform.nvim be lazy-loadable again
    Ref: https://github.com/nvim-lua/kickstart.nvim/commit/b36d84ddf0b88ab71593d25bf68b48cae9d578eb

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -702,7 +702,8 @@ require('lazy').setup({
 
   { -- Autoformat
     'stevearc/conform.nvim',
-    lazy = false,
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
     keys = {
       {
         '<leader>f',
