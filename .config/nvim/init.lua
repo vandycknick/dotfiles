92eb6510f5c47db97f8ce03517f58f43b591fa53
Nick Van Dyck <vandyck.nick@outlook.com>
2024-12-06 08:06:46 +0100

    docs(nvim): remove unused comment

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -234,10 +234,7 @@ require('lazy').setup({
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
-  -- Here is a more advanced example where we pass configuration
-  -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
-  --    require('gitsigns').setup({ ... })
-  --
+  -- Github: https://github.com/lewis6991/gitsigns.nvim
   -- See `:help gitsigns` to understand what the configuration keys do
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
