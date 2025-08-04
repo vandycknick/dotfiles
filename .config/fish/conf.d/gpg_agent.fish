c41df671c9b583185257343ceb01baf6ff83e31e
Nick Van Dyck <vandyck.nick@outlook.com>
2025-08-04 08:57:21 +0200

    feat: use gpg for ssh

--- .config/fish/conf.d/gpg_agent.fish
+++ .config/fish/conf.d/gpg_agent.fish
@@ -3,4 +3,5 @@ status is-interactive || exit
 set -Ux GNUPGHOME "$XDG_DATA_HOME/gnupg"
 
 set -x GPG_TTY (tty)
+set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
 gpgconf --launch gpg-agent
--- .config/fish/conf.d/ssh_agent.fish
+++ .config/fish/conf.d/ssh_agent.fish
@@ -1,5 +1,9 @@
 status is-interactive || exit
 
-set --query ssh_env || set --global ssh_env "$XDG_CACHE_HOME/ssh/environment"
+# TODO: Make this configurable at soem point.
+if not set -q ssh_agent_enabled
+    exit
+end
 
+set --query ssh_env || set --global ssh_env "$XDG_CACHE_HOME/ssh/environment"
 ssh_agent_start --ssh-add
