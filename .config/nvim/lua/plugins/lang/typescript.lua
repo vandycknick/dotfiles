3b29ca886687917d2f58ff208a794ef6a2d7b635
Nick Van Dyck <vandyck.nick@outlook.com>
2025-09-27 11:07:17 +0200

    feat(nvim): better ts_ls and deno_ls compatibility

--- .config/nvim/lua/plugins/lang/typescript.lua
+++ .config/nvim/lua/plugins/lang/typescript.lua
@@ -4,12 +4,17 @@ return {
     opts = {
       servers = {
         ts_ls = {
-          -- root_dir = function(bufnr)
-          --   local startpath = vim.api.nvim_buf_get_name(bufnr)
-          --   require('lspconfig.util').root_pattern('tsconfig.json', 'jsconfig.json', 'package.json')(startpath)
-          -- end,
-          workspace_required = true,
-          root_markers = { 'tsconfig.json', 'jsconfig.json', 'package.json' },
+          root_dir = function(bufnr, on_dir)
+            -- The project root is where the LSP can be started from
+            -- As stated in the documentation above, this LSP supports monorepos and simple projects.
+            -- We select then from the project root, which is identified by the presence of a package
+            -- manager lock file.
+            local root_markers = { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml' }
+            local project_root = vim.fs.root(bufnr, root_markers) or ''
+            if project_root ~= '' then
+              on_dir(project_root)
+            end
+          end,
           settings = {
             typescriptreact = {
               -- https://github.com/typescript-language-server/typescript-language-server?tab=readme-ov-file#inlay-hints-textdocumentinlayhint
